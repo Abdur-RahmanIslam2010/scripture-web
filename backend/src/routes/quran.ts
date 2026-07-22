cb305403f2e6d6954ab984ae0332dfc8ec04a9d3
Abdur-RahmanIslam2010 <abdelrahmanislam116@gmail.com>
2026-07-22 21:39:05 +0300

    Made the Quran endpoint also send entire chapters

--- backend/src/routes/quran.ts
+++ backend/src/routes/quran.ts
@@ -4,24 +4,32 @@ const router = express.Router();
 const url = 'https://api.alquran.cloud/v1';
 
 router.get(
-    '/verse/:verseReference',
+    '/:reference',
     async (req: express.Request, res: express.Response) => {
-        const verseReference = req.params.verseReference;
+        const reference = req.params.reference;
 
-        if (!verseReference || typeof verseReference !== 'string') {
+        if (!reference || typeof reference !== 'string') {
             return res.status(404).json({ error: "Verse reference not found." });
         }
 
-        const [chapter, verse] = verseReference.split(":");
-        if (!verse || !chapter) {
+        const [chapter, verse] = reference.split(":");
+        if (!chapter) {
             return res.status(404).json({ error: "Verse or chapter not found." });
         }
-        const verseNumber = parseInt(verse);
+        if (!verse) {
+            const response = await fetch(`${url}/surah/${chapter}`);
+            const data = await response.json();
+            console.log(data);
+            return res.status(200).send(data);
+        } else {
+            const verseNumber = parseInt(verse);
+            const response = await fetch(`${url}/surah/${chapter}?offset=${verseNumber - 1}&limit=1`);
+            const data = await response.json();
+            console.log(data);
+            return res.status(200).send(data);
+        }
 
-        const response = await fetch(`${url}/surah/${chapter}?offset=${verseNumber - 1}&limit=1`);
-        const data = await response.json();
-        console.log(data);
-        res.status(200).send(data);
+
     }
 );
 
